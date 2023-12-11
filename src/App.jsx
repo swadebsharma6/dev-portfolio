66cc8b155e0aff64d8069e2f05f5936bc2264123
swadebsharma6 <swadebriva2012@gmail.com>
2023-12-11 11:25:35 +0600

    initializedthe project first time

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+ <div>
+    <h2 className='text-2xl text-center text-primary font-bold'>My Personal website</h2>
+ </div>
+  )
+}
+
+export default App
